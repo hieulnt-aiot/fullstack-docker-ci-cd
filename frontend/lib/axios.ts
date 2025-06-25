11a6bfcc308b852955641adbea0e31bc0a33c999
hieulnt-aiot <hieulnt@aiot-global.com>
2025-06-25 17:12:47 +0700

    fix: [refactor] fe 3

--- frontend/lib/axios.ts
+++ frontend/lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: typeof window !== 'undefined' ? window.env?.NEXT_PUBLIC_API_URL : '',
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
 });
 
 export default instance;
