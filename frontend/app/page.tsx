d5782881261734e682c610668e5bc0902f16ce48
hieulnt-aiot <hieulnt@aiot-global.com>
2025-06-26 11:08:26 +0700

    fix: revert to old commit

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -3,11 +3,13 @@
 import { useEffect, useState } from 'react';
 import Link from 'next/link';
 import { getAndUpdateCounter } from '@/api/counter';
+import instance from '@/lib/axios';
 
 export default function HomePage() {
   const [count, setCount] = useState<number | null>(null);
   const [loading, setLoading] = useState(true);
-  
+
+  console.log('instance:' + instance.getUri);
   useEffect(() => {
     const fetchCounter = async () => {
       try {
